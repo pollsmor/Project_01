4c78239c19be3bbf6ce95f6957573050f7d82b94
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-25 14:59:58 -0500

    changed name "source" to "origin"

--- utl/search.py
+++ utl/search.py
@@ -13,7 +13,7 @@ query_patterns = {
     'fuel mass':re.compile('how much (fuel|mass)'),
     'destination':re.compile('to (reach |flyby |(get|fly) to )?[\-a-z0-9]+'),
     'method':re.compile('to (reach|flyby|(get|fly) to)'),
-    'source':re.compile('from [\-a-z0-9]+'),
+    'origin':re.compile('from [\-a-z0-9]+'),
     'engine':re.compile('using (merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+'),
     'fuel':re.compile('and [0-9]*.?[0-9]+ ?(kg| kilograms) (of )?fuel'),
     'time':re.compile('in [0-9]+ years')
@@ -22,7 +22,7 @@ query_patterns = {
 reduction_patterns = {
     'destination':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
     'method':(re.compile('(reach|flyby|(get|fly) to)$'), lambda s: s if s == "flyby" else "reach"),
-    'source':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
+    'origin':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
     'engine':(re.compile('((merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+)$'), lambda s: s),
     'fuel':(re.compile('[0-9]*.?[0-9]+'), lambda f: float(f)),
     'time':(re.compile('[0-9]+'), lambda i: int(i))
@@ -72,7 +72,7 @@ def _parse(query):
     else:
         raise BadQuery('Query error: question type not recognized')
 
-    set_category(category = 'source', default = 'earth')
+    set_category(category = 'origin', default = 'earth')
     set_category(category = 'destination')
     set_category(category = 'method', default = 'reach')
     set_category(category = 'engine')
