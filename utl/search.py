63e3e6bab35d73f7b052a37b58c9c3ad4fcde423
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-25 10:07:06 -0500

    added reduction patterns for another stage of processing in _parse

--- utl/search.py
+++ utl/search.py
@@ -12,11 +12,20 @@ query_patterns = {
     'travel time':re.compile('(time|how long)'),
     'fuel mass':re.compile('how much (fuel|mass)'),
     'destination':re.compile('to (reach |flyby |(get|fly) to )?[\-a-z0-9]+'),
-    'method':re.compile('to (reach |flyby |(get|fly) to )'),
+    'method':re.compile('to (reach|flyby|(get|fly) to)'),
     'source':re.compile('from [\-a-z0-9]+'),
     'engine':re.compile('using (merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+'),
     'fuel':re.compile('and [0-9]*.?[0-9]+ ?(kg| kilograms) (of )?fuel'),
     'time':re.compile('in [0-9]+ years')
+}
+#reduces a query parameter to raw content using the regular expression and the given function
+reduction_patterns = {
+    'destination':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
+    'method':(re.compile('(reach|flyby|(get|fly) to)$'), lambda s: s if s == "flyby" else "reach"),
+    'source':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
+    'engine':(re.compile('((merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+)$'), lambda s: s),
+    'fuel':(re.compile('[0-9]*.?[0-9]+'), lambda f: float(f)),
+    'time':(re.compile('[0-9]+'), lambda i: int(i))
 }
 
 class BadQuery(Exception):
@@ -44,7 +53,9 @@ def _parse(query):
         # finds parameter in query using corresponding regex, adds to parameter dictionary
         match = re.search(query_patterns[category], query)
         if match:
-            params[category] = substr(match, query)
+            intermediate = substr(match, query)
+            final_raw = substr(re.search(reduction_patterns[category][0], intermediate), intermediate)
+            params[category] = reduction_patterns[category][1](final_raw)
         elif default != '':
             params[category] = default
         else: # if the default action isn't specified, assumes error
@@ -61,9 +72,21 @@ def _parse(query):
     else:
         raise BadQuery('Query error: question type not recognized')
 
-    set_category(category = 'source', default = 'from earth')
+    set_category(category = 'source', default = 'earth')
     set_category(category = 'destination')
     set_category(category = 'method', default = 'reach')
     set_category(category = 'engine')
+
+
     
     return params
+
+
+test_queries = [
+    "how long to reach kepler-10c using merlin 1d and 1000kg of fuel",
+    "how long to get to kepler-10d using RS-25 and 1000.2kg of fuel",
+    "how much fuel to flyby kepler-10d using RS-25 in 10 years"
+]
+
+for item in test_queries:
+    print(_parse(item))
