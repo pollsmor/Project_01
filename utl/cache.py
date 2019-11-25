a3f803402eaca336c26c67204270d9230165b754
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-25 18:56:34 -0500

    untested cache db init

--- utl/cache.py
+++ utl/cache.py
@@ -4,12 +4,39 @@ __dbfile__ = 'data/cache.db'
 
 # def search(query):
     
+def INIT(replace=0):
+    db = sqlite3.connect(__dbfile__)
+    tables = ['engines','planets','timeqs','massqs']
+    if replace:
+        for table in tables:
+            db.execute('drop table ?;', table)
+
+    db.execute(
+        '''create table engines 
+        (name text primary key, 
+        mass numeric, impulse numeric, thrust numeric
+        img text);'''
+    )
+    db.execute(
+        '''create table planets
+        (name text primary key,  
+        distance numeric, ra numeric, dec numeric);'''
+    )
+    db.execute(
+        '''create table queries 
+        (origin text, 
+        method text, 
+        destination text,
+        engine text, 
+        fuel integer, 
+        time integer);'''
+    )
+    db.commit()
+    db.close()
+
+
 def store(results):
-    if results['type'] == 'travel time':
-        _insert(table='timeqs', values=results['results'])
-    elif results['type'] == 'fuel mass':
-        _insert(table='massqs', values=results['results'])
-    
+    _insert(table='queries', values=results['results'])
     _insert(table='engines', values=results['engine'])
     _insert(table='planets', values=results['origin'])
     _insert(table='planets', values=results['destination'])
