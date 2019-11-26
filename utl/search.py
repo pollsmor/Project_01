951add3b55b6ec74c2d7bbb4a3b83a512b600ff5
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-26 10:04:50 -0500

    fixed regexes

--- utl/search.py
+++ utl/search.py
@@ -7,25 +7,35 @@
 import re
 import api_bus
 
+#included separately due to frequency of use and modification
+planet_pattern = '[a-z]+(\-[0-9]+ ?[a-z ])?'
+rocket_pattern = '(Merlin |Rocketdyne |BMW |S[0-9]\.)?[\-a-zA-Z0-9]+'
+
 # maps query parameters to regular expessions
 query_patterns = {
-    'travel time':re.compile('(time|how long)'),
-    'fuel mass':re.compile('how much (fuel|mass)'),
-    'destination':re.compile('to (reach |flyby |(get|fly) to )?[\-a-z0-9]+'),
-    'method':re.compile('to (reach|flyby|(get|fly) to)'),
-    'origin':re.compile('from [\-a-z0-9]+'),
-    'engine':re.compile('using (merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+'),
-    'fuel':re.compile('and [0-9]*.?[0-9]+ ?(kg| kilograms) (of )?fuel'),
-    'time':re.compile('in [0-9]+ years')
+    'travel time':re.compile('(time|how long)', re.IGNORECASE),
+    'fuel mass':re.compile('how much (fuel|mass)', re.IGNORECASE),
+    'destination':re.compile('to (reach |flyby |(get|fly) to )?%s' % planet_pattern, re.IGNORECASE),
+    'method':re.compile('to (reach|flyby|(get|fly) to)', re.IGNORECASE),
+    'origin':re.compile('from %s' % planet_pattern, re.IGNORECASE),
+    'engine':re.compile('(using|with) %s' % rocket_pattern, re.IGNORECASE),
+    'fuel':re.compile('(and|using) [0-9]*.?[0-9]+ ?(kg| kilograms) (of )?fuel', re.IGNORECASE),
+    'time':re.compile('in [0-9]+ years', re.IGNORECASE)
 }
 #reduces a query parameter to raw content using the regular expression and the given function
 reduction_patterns = {
-    'destination':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
-    'method':(re.compile('(reach|flyby|(get|fly) to)$'), lambda s: s if s == "flyby" else "reach"),
-    'origin':(re.compile('([\-a-z0-9]+)$'), lambda s: s),
-    'engine':(re.compile('((merlin |rocketdyne |bmw |S[0-9]\.)?[\-a-z0-9]+)$'), lambda s: s),
-    'fuel':(re.compile('[0-9]*.?[0-9]+'), lambda f: float(f)),
-    'time':(re.compile('[0-9]+'), lambda i: int(i))
+    'method':(re.compile('(reach|flyby|(get|fly) to)$'), 
+        lambda s: s if s == "flyby" else "reach"),
+    'destination':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE), 
+        lambda s: s[1:-1]),
+    'origin':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE), 
+        lambda s: s[1:-1]),
+    'engine':(re.compile('( %s)$' % rocket_pattern, re.IGNORECASE), 
+        lambda s: s[1:]),
+    'fuel':(re.compile('[0-9]*.?[0-9]+'), 
+        lambda f: float(f)),
+    'time':(re.compile('[0-9]+'), 
+        lambda i: int(i))
 }
 
 class BadQuery(Exception):
@@ -55,7 +65,9 @@ def _parse(query):
         if match:
             intermediate = substr(match, query)
             final_raw = substr(re.search(reduction_patterns[category][0], intermediate), intermediate)
-            params[category] = reduction_patterns[category][1](final_raw)
+            final = reduction_patterns[category][1](final_raw)
+            params[category] = final
+            # print('Reduced \"%s\" to \"%s\"' % (intermediate, final))
         elif default != '':
             params[category] = default
         else: # if the default action isn't specified, assumes error
@@ -81,9 +93,21 @@ def _parse(query):
     
     return params
 
+def dict_print(d):
+    print('{')
+    for item in d.items():
+        print('\t%s: %s' % item)
+    print('}')
 
 test_queries = [
-    "how long to reach kepler-10c using merlin 1d and 1000kg of fuel",
-    "how long to get to kepler-10d using RS-25 and 1000.2kg of fuel",
-    "how much fuel to flyby kepler-10d using RS-25 in 10 years"
+    "how long to reach kepler-10 c using merlin 1d and 1000kg of fuel",
+    "how long to get to kepler-10 d using RS-25 and 1000.2kg of fuel",
+    "how much fuel to flyby kepler-10 d using RS-25 in 10 years",
+    "how long to reach kepler-10 from kepler-11 using merlin 1d and 10kg of fuel"
 ]
+
+for query in test_queries:
+    try:
+        dict_print(search(query))
+    except BadQuery as badness:
+        print(badness)
