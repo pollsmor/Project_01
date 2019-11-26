445bf298f2d53eb0104783e3baad90c060572bfe
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-26 10:09:15 -0500

    merged chache from master

--- utl/cache.py
+++ utl/cache.py
@@ -4,22 +4,53 @@ __dbfile__ = 'data/cache.db'
 
 # def search(query):
     
+def INIT(replace=0):
+    db = sqlite3.connect(__dbfile__)
+    tables = ['engines','planets','timeqs','massqs']
+    if replace:
+        for table in tables:
+            db.execute('drop table ?;', table)
+
+    db.execute(
+        '''create table engines 
+        (name text primary key, 
+        mass numeric, impulse numeric, thrust numeric
+        img text);'''
+    )
+    db.execute(
+        '''create table planets
+        (name text primary key,  
+        distance numeric, ra numeric, dec numeric);'''
+    )
+    db.execute(
+        '''create table queries 
+        (origin text, 
+        method text, 
+        destination text,
+        engine text, 
+        fuel integer, 
+        time integer);'''
+    )
+    db.commit()
+    db.close()
+
+
 def store(results):
-    if results['type'] == 'travel time':
-        _insert(table='timeqs', values=(results['source']['name'], results['source']['name'], results['engine']['name'], results['fuel'], results['time']))
-    elif results['type'] == 'fuel mass':
-        _insert(table='massqs', values=(results['source']['name'], results['source']['name'], results['engine']['name'], results['time'], results['fuel']))
-    _insert(table='engines', values=(results['source']))
-    _insert(table='planets', values=())
+    _insert(table='queries', values=results['results'])
+    _insert(table='engines', values=results['engine'])
+    _insert(table='planets', values=results['origin'])
+    _insert(table='planets', values=results['destination'])
 
 def _insert(*args, **kwargs):
-    db = sqlite3.connect(__dbfile__)
-
     table = kwargs['table']
-    cols = "("
-    contents = kwargs['values']
-    argfs = ('?,' * len(contents))[:-1]
+    values = kwargs['values'].items()
 
-    db.execute(f'insert into {table} values ({argfs});', contents)
+    # forms an ordering between columns and values
+    # removes necessity for correctly ordering entries in values
+    cols = '(' + ','.join([item[0] for item in values]) + ')'
+    argfs = '(' + ','.join([str(item[1]) for item in values]) + ')'
+
+    db = sqlite3.connect(__dbfile__)
+    db.execute('insert or ignore into %s %s values %s' % (table, cols, argfs))
     db.commit()
     db.close()
