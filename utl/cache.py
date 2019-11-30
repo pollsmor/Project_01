e1602012718efc2367eca134847580666b171aa8
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-29 20:46:46 -0500

    rewrote _search_item, planning for search

--- utl/cache.py
+++ utl/cache.py
@@ -2,50 +2,47 @@ import sqlite3
 
 __dbfile__ = 'data/cache.db'
 
+# defines default order of selection
+default_orders = {
+    'engines': ['name', 'mass', 'impulse', 'exhaust', 'thrust', 'img', 'propellant'],
+    'planets': ['name', 'distance', 'ra', 'dec'] 
+}
+
 def INIT(replace=0):
     db = sqlite3.connect(__dbfile__)
-    schemaf = open('utl/db_schema.txt','r')
-    schema = schemaf.read()
-    print(schema)
-    schema = schema.split('\n')
+    with open('utl/db_schema.txt','r') as schemaf:
+        schema = schemaf.read().split('\n')
     if replace:
         for cmd in schema[3:]:
             try:
-                print(cmd)
                 db.execute(cmd)
             except sqlite3.OperationalError as ex:
                 print(ex)
     for cmd in schema[:3]:
-        print(cmd)
         db.execute(cmd)
     db.commit()
     db.close()
 
-def _search_one(*args, **kwargs):
+def search(query):
+    # should: compare the query against the entries in queries
+    # if the match isn't perfect, find extant entries in planets and engines
+    # if matches aren't found, value False
+    return query
+
+def _search_item(*args, **kwargs):
     table = kwargs['table']
-    query = kwargs['data'].items()
+    name = kwargs['name']
 
-    # generates selection order
-    cols = [item[0] for item in query]
-    cols = '(%s)' % (','.join(cols)) # generates "(col1,col2,...)"
-    
-    # generates 'WHERE' clause
-    where = ['(%s=?)' % item[0] for item in query] # generates ['(col1=?)','(col2=?)',...]
-    where = ' AND '.join(where) # converts to "(col1=?) AND (col2=?) AND ..."
-
-    args = tuple([item[1] for item in query])
-    
+    order = '(%s)' % ','.join(default_orders[table])
     db = sqlite3.connect(__dbfile__)
-    result = db.execute('select %s from %s where %s;' % (cols, table, where), args)
+    result = db.execute('select ? from ? where (name = ?);', (order, table, name))
     result = [item for item in result]
     if len(result):
-        return result[1]
+        return dict(zip(default_orders[table], result[0])) # converts result to dictionary
     else:
         return False
-    db.close()
 
-
-def _store(results):
+def store(results):
     _insert(table='queries', values=results['results'])
     _insert(table='engines', values=results['engine'])
     _insert(table='planets', values=results['origin'])
