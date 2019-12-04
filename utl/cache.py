154da9ae28cff14c1505b9aba392e47f037e5168
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-12-03 20:58:49 -0500

    merging with search-dev

--- utl/cache.py
+++ utl/cache.py
@@ -1,13 +1,5 @@
 import sqlite3
 
-"""Contains functions that handle the caching of the website.
-
-Functions:
-- INIT(replace): creates a database file that stores the cached data
-- store(results): stores searched results in the database for easy access later
-- _insert(*args, **kwargs): what store() uses to insert into the database file
-"""
-
 __dbfile__ = 'data/cache.db'
 
 # defines default order of selection
@@ -53,33 +45,6 @@ def search(query): # searches db for query
             return False
     
     db = sqlite3.connect(__dbfile__)
-    tables = ['engines','planets','timeqs','massqs']
-    if replace:
-        for table in tables:
-            db.execute('drop table ?;', table)
-
-    db.execute(
-        '''create table engines
-        (name text primary key,
-        mass numeric, impulse numeric, thrust numeric
-        img text);'''
-    )
-    db.execute(
-        '''create table planets
-        (name text primary key,
-        distance numeric, ra numeric, dec numeric);'''
-    )
-    db.execute(
-        '''create table queries
-        (origin text,
-        method text,
-        destination text,
-        engine text,
-        fuel integer,
-        time integer);'''
-    )
-    db.commit()
-    db.close()
 
     result = {}
     result['origin'] = search_item(table='planets',name=query['origin'])
--- utl/search.py
+++ utl/search.py
@@ -5,94 +5,155 @@
 
 
 import re
-import utl.api_bus
-
-"""Contains the search functionality needed by the front end developer.
-
-Exceptions:
-- QueryFailure: custom exception that gets thrown when the query is invalid
-
-Functions:
-- search(query): takes the dictionary from _parse and requests info from the three APIs, then returns that info.
-- _parse(query): takes a query and returns a dictionary of the parameters extracted from said query.
-"""
+from api_bus import wolfram, wikipedia, exoplanets, QueryFailure
+from cache import search as cachesearch, insert
 
 #included separately due to frequency of use and modification
-planet_pattern = '[a-z]+(\-[0-9]+ ?[a-z ])?'
+planet_pattern = '[a-z]+(\-[0-9]+ ?([a-z][^a-z])?)?'
 rocket_pattern = '(Merlin |Rocketdyne |BMW |S[0-9]\.)?[\-a-zA-Z0-9]+'
-
+parsec = int(3.086 * (10 ** 13))
 # maps query parameters to regular expessions
 query_patterns = {
-    'travel time':re.compile('(time|how long)', re.IGNORECASE),
-    'fuel mass':re.compile('how much (fuel|mass)', re.IGNORECASE),
-    'destination':re.compile('to (reach |flyby |(get|fly) to )?%s' % planet_pattern, re.IGNORECASE),
-    'method':re.compile('to (reach|flyby|(get|fly) to)', re.IGNORECASE),
-    'origin':re.compile('from %s' % planet_pattern, re.IGNORECASE),
-    'engine':re.compile('(using|with) %s' % rocket_pattern, re.IGNORECASE),
-    'fuel':re.compile('(and|using) [0-9]*.?[0-9]+ ?(kg| kilograms) (of )?fuel', re.IGNORECASE),
-    'time':re.compile('in [0-9]+ years', re.IGNORECASE)
+    'travel time':re.compile('(time|how long)',
+        re.IGNORECASE),
+    'fuel mass':re.compile('how much (fuel|mass)',
+        re.IGNORECASE),
+    'destination':re.compile('to (reach |flyby |(get|fly) to )?%s' % planet_pattern,
+        re.IGNORECASE),
+    'method':re.compile('to (reach|flyby|(get|fly) to)', 
+        re.IGNORECASE),
+    'origin':re.compile('from %s' % planet_pattern, 
+        re.IGNORECASE),
+    'engine':re.compile('(using|with) %s' % rocket_pattern, 
+        re.IGNORECASE),
+    'fuel':re.compile('(and|using) [0-9]*.?[0-9]+( ?kg| kilograms| tons) of fuel',
+        re.IGNORECASE),
+    'time':re.compile('in [0-9]+ (years|millenia)',
+        re.IGNORECASE)
 }
 #reduces a query parameter to raw content using the regular expression and the given function
-reduction_patterns = {
+reductions = {
     'method':(re.compile('(reach|flyby|(get|fly) to)$'), 
-        lambda s: s if s == "flyby" else "reach"),
-    'destination':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE), 
-        lambda s: s[1:-1]),
-    'origin':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE), 
-        lambda s: s[1:-1]),
-    'engine':(re.compile('( %s)$' % rocket_pattern, re.IGNORECASE), 
-        lambda s: s[1:]),
-    'fuel':(re.compile('[0-9]*.?[0-9]+'), 
-        lambda f: float(f)),
-    'time':(re.compile('[0-9]+'), 
-        lambda i: int(i))
+        lambda s, m: s if s == 'flyby' else 'reach'),
+    'destination':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE),
+        lambda s, m: s[1:-1]),
+    'origin':(re.compile('( %s)$' % planet_pattern, re.IGNORECASE),
+        lambda s, m: s[1:-1]),
+    'engine':(re.compile('( %s)$' % rocket_pattern, re.IGNORECASE),
+        lambda s, m: s[1:]),
+    'fuel':(re.compile('[0-9]*.?[0-9]+'),
+        lambda f, m: float(f) * 1000 if 'tons' in m else 1),
+    'time':(re.compile('[0-9]+'),
+        lambda i, m: float(i) * 1000 if 'millenia' in m else 1)
+}
+# mathematical expressions to send to Wolfram|Alpha
+expressions = {
+    'distance':'Sqrt{{ {dist1}^2 + {dist2}^2 - 2*{dist1}*{dist2}Sin{{{ra1} degrees}}Sin{{{ra2} degrees}}Cos{{{decdiff} degrees}} + Cos{{{ra1} degrees}}Cos{{{ra2} degrees}} }}',
+    'reach': {
+        'fuel':'{end}*Exp{{(2*({dist})/({exh}*{time}))}}',
+        'time':'2*({dist})/({exh}*Ln{{({start})/({end})}})'
+    },
+    'flyby': {
+        'fuel':'{end}*Exp{{(({dist})/({exh}*{time}))}}',
+        'time':'({dist})/({exh}*Ln{{({start})/({end})}})'
+    }
 }
 
 class BadQuery(Exception):
     pass
 
 def search(query):
-    """Takes the dictionary from _parse and requests info from the three APIs, then returns that info."""
     query = _parse(query)
-    # -- REPLACE -- # with API requests
-    return query # -- REPLACE -- # with results
+    print(query)
+    result = cachesearch(query)
+    if type(result) != dict:
+        return result
+    
+    # uses APIs to find data not found in cache
+    if not result['engine']:
+        try:
+            result['engine'] = wikipedia(query['engine'])
+            insert(table='engines', values=result['engine'])
+        except QueryFailure as qf:
+            raise
+    if not result['origin']:
+        try:
+            result['origin'] = exoplanets(query['origin'])
+            insert(table='planets', values=result['origin'])
+        except QueryFailure as qf:
+            raise
+    if not result['destination']:
+        try:
+            result['destination'] = exoplanets(query['destination'])
+            insert(table='planets', values=result['destination'])
+        except QueryFailure as qf:
+            raise
+
+    # Substitutes numbers into distance expression
+    distance = expressions['distance'].format(
+        dist1 = int(result['origin']['distance'] * parsec),
+        dist2 = int(result['destination']['distance'] * parsec),
+        ra1 = result['origin']['ra'],
+        ra2 = result['destination']['ra'],
+        decdiff = int(result['origin']['dec'] - result['destination']['dec'])
+    )
+
+    # generates full expression corresponding to query
+    if query['type'] == 'travel time':
+        expr = expressions[query['method']]['time'].format(
+            dist=distance,
+            exh=result['engine']['exhaust'],
+            end=result['engine']['mass'] + 10,
+            start = query['fuel'] + result['engine']['mass'] + 10
+        )
+    elif query['type'] == 'fuel mass':
+        expr = expressions[query['method']]['fuel'].format(
+            dist=distance,
+            exh=result['engine']['exhaust'],
+            end=result['engine']['mass'] + 10,
+            time=query['time']
+        )
+    
+    try:
+        query['result'] = wolfram(expr)
+        return query
+    except QueryFailure as qf:
+        raise
+
 
 
 def _parse(query):
-    """Takes a query and returns a dictionary of the parameters extracted from said query."""
-    query = query.lower()
+    # query = query.lower()
     params = {}
     params['query'] = query
 
-    # LOCAL FUNCTIONS
     def substr(match, string): # substring using span in Match object
         return string[match.span()[0]:match.span()[1]]
 
     def set_category(*args, **kwargs):
+        # Finds keywords for a category in query string
+        # Adds corresponding data to parameters dictionary
         category = kwargs['category']
         default = kwargs['default'] if ('default' in kwargs) else ''
 
-        # finds parameter in query using corresponding regex, adds to parameter dictionary
         match = re.search(query_patterns[category], query)
         if match:
             intermediate = substr(match, query)
-            final_raw = substr(re.search(reduction_patterns[category][0], intermediate), intermediate)
-            final = reduction_patterns[category][1](final_raw)
+            final_raw = substr(re.search(reductions[category][0], intermediate), intermediate)
+            final = reductions[category][1](final_raw, intermediate)
             params[category] = final
-            # print('Reduced \"%s\" to \"%s\"' % (intermediate, final))
         elif default != '':
             params[category] = default
         else: # if the default action isn't specified, assumes error
-            raise BadQuery(f'Query error: {category} not present')
+            raise BadQuery('Query error: %s not present' % category)
 
-    # PROCESSING
     # Determine question type
     if re.search(query_patterns['travel time'], query):
         params['type'] = 'travel time'
         set_category(category = 'fuel')
     elif re.search(query_patterns['fuel mass'], query):
         params['type'] = 'fuel mass'
-        set_category(category = 'time', default = 'minimal')
+        set_category(category = 'time')
     else:
         raise BadQuery('Query error: question type not recognized')
 
@@ -101,25 +162,4 @@ def _parse(query):
     set_category(category = 'method', default = 'reach')
     set_category(category = 'engine')
 
-
-
     return params
-
-def dict_print(d):
-    print('{')
-    for item in d.items():
-        print('\t%s: %s' % item)
-    print('}')
-
-test_queries = [
-    "how long to reach kepler-10 c using merlin 1d and 1000kg of fuel",
-    "how long to get to kepler-10 d using RS-25 and 1000.2kg of fuel",
-    "how much fuel to flyby kepler-10 d using RS-25 in 10 years",
-    "how long to reach kepler-10 from kepler-11 using merlin 1d and 10kg of fuel"
-]
-
-for query in test_queries:
-    try:
-        dict_print(search(query))
-    except BadQuery as badness:
-        print(badness)
